0a4c46f863d9946315876808e60cbe8a9e2a78f6
metleeha <nodongth@gmail.com>
2020-12-22 23:23:15 +0900

    leetcode: 7

---
+++ leetcode/7.Reverse_Integer.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/reverse-integer/
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x > 0:
+            value = int(str(x)[::-1])
+        else: 
+            value = -1 * int(str(x*-1)[::-1])
+        
+        if value not in range(-2**31, 2**31):
+            value = 0
+        
+        return value
