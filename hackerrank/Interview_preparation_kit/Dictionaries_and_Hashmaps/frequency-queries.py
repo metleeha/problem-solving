e12a7ddc85f284b27af05ccdca419bef20bc9068
metleeha <nodongth@gmail.com>
2021-01-13 21:40:16 +0900

    210113

---
+++ hackerrank/Interview_preparation_kit/Dictionaries_and_Hashmaps/frequency-queries.py
@@ -0,0 +1,7 @@
+'''
+You are given  queries. Each query is of the form two integers described below:
+-  : Insert x in your data structure.
+-  : Delete one occurence of y from your data structure, if present.
+-  : Check if any integer is present whose frequency is exactly . If yes, print 1 else 0.
+'''
+
